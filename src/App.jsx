8e198f586404bef16b25d5b30abd4a98978956fc
Valentine Zvikaramba <zvkish001@myuct.ac.za>
2024-01-23 13:30:53 +0200

    add chakra-ui and react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import Launch from './components/launch';
+
+//import installed packages and libraries
+import React from 'react';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+const App = () =>{
+
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Launch/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/launch.jsx
@@ -0,0 +1,12 @@
+//import local resources and assets
+
+//import installed packages and libraries 
+import React from "react";
+
+const Launch = () => {
+  return (
+    <div>launch</div>
+  )
+}
+
+export default Launch
